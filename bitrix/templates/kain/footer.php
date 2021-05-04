7a483ec42b8fab56d91f98d5ee2e2328134ef84b
Shuman Denis <romlekus666@gmail.com>
2021-05-04 12:41:11 +0400

    my second commit

---
+++ bitrix/templates/kain/footer.php
@@ -0,0 +1,25 @@
+<!----------------------Footer------------------->
+<footer>
+    <div class="footer_text">
+        <h4>ООО «Сервис Айти»</h4>
+        <p>Популярные разделы:
+            <a href="#">Купить 1С</a> |
+            <a href="#">Сопровождение 1С</a> |
+            <a href="">Сервисы 1С</a> |
+            <a href="">Обучение 1С</a> |
+            <a href="">Обучение 1С</a>|
+            <a href="">Акции и спецпредложения</a></p>
+        <p>«Дом сайтов» - <u>создание сайтов</u> и <u>продвижение</u> </p>
+    </div>
+    <img src="img/icon-email.png" alt="">
+    <div class="mini_menu">
+        <p>Популярные разделы:
+            <a href="#">Главная</a> |
+            <a href="#">Контакты</a> |
+            <a href="#">Карта сайта</a>
+        </p>
+    </div>
+</footer>
+<script src="<?=SITE_TEMPLATE_PATH?> script.js"></script>
+</body>
+</html>
